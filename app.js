b5222ea87273556217104aa10599010054f0ae49
AndriiChemerys <andriichemerys@gmail.com>
2021-01-28 19:03:30 +0100

    Creating quotesOnPageAmount limit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 document.querySelector('.get-quotes').addEventListener('click', getQuotes);
 let output = '';
+let count = 0;
+const quotesOnPageAmount = 5;
 // const number = document.querySelector('input[type="number"]').value;
 
 function getQuotes(e) {
@@ -10,9 +12,15 @@ function getQuotes(e) {
         .then(data => {
             const maxQuotesNum = 1642;
             let number = Math.floor(Math.random() * Math.floor(maxQuotesNum));
-            console.log(number + ' - ' + data[number].text)
+            count++;
+
             output += `<li>${data[number].text}</li>`;
             document.querySelector('.quotes').innerHTML = output;
+
+            // Clearing quotes ul element when quotes amount is bigger than quotesOnPageAmount          
+            if(count % quotesOnPageAmount == 0) {
+                output = ` `;
+            }
         });
 
     e.preventDefault();
